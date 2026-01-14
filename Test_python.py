1be40c66fd4bf62603b5833fe713e88cf3fdca47
Deepak Dhanapal <deepakdhanapal94.com>
2026-01-14 00:35:38 +0000

    The small change of script in python file

--- Test_python.py
+++ Test_python.py
@@ -1,1 +1,5 @@
-print("I am hungry")
+test = input("Is this your first script?:")
+if test == "yes":
+    print("Congratulations!!! All the best")
+else:
+    print("You are already started learning. Thats a good sign!!!")   
